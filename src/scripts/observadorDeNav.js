74217584b6b58b3352dde91a886bb9ab302b7923
Alvaro Bravo <bravoalva19@gmail.com>
2025-11-18 01:47:29 -0300

    Agregando navegacion

---
+++ src/scripts/observadorDeNav.js
@@ -0,0 +1,22 @@
+const secciones = document.querySelectorAll("div[id]")   // <-- ahora usa divs
+const links = document.querySelectorAll("nav a[data-section]")
+console.log('Paso por aca')
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const id = entry.target.id
+
+        links.forEach(link => link.classList.remove("active"))
+
+        const linkActivo = document.querySelector(`nav a[data-section="${id}"]`)
+        linkActivo?.classList.add("active")
+      }
+    })
+  },
+  {
+    threshold: 0.5, // 50% visible
+  }
+)
+
+secciones.forEach(sec => observer.observe(sec))
