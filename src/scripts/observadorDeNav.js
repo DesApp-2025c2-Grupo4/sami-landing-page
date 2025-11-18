840ad5dac389cf72efcc0a77c0e60d81bb7522ce
Alvaro Bravo <bravoalva19@gmail.com>
2025-11-18 18:52:10 -0300

    Agregando manejo de url

--- src/scripts/observadorDeNav.js
+++ src/scripts/observadorDeNav.js
@@ -1,21 +1,25 @@
-const secciones = document.querySelectorAll("div[id]")   // <-- ahora usa divs
-const links = document.querySelectorAll("nav a[data-section]")
+const secciones = document.querySelectorAll("div[id]");
+const links = document.querySelectorAll("nav a[data-section]");
+
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        const id = entry.target.id
+        const id = entry.target.id;
 
-        links.forEach(link => link.classList.remove("active"))
+        // Cambiar URL sin recargar
+        history.replaceState(null, "", `#${id}`);
 
-        const linkActivo = document.querySelector(`nav a[data-section="${id}"]`)
-        linkActivo?.classList.add("active")
+        // Actualizar clases activas
+        links.forEach(link => link.classList.remove("active"));
+        const linkActivo = document.querySelector(`nav a[data-section="${id}"]`);
+        linkActivo?.classList.add("active");
       }
-    })
+    });
   },
   {
     threshold: 0.2,
   }
-)
+);
 
-secciones.forEach(sec => observer.observe(sec))
+secciones.forEach(sec => observer.observe(sec));
